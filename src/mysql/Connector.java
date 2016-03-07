b4243590f4d3100bbb314fa546137f43547d33a2
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-03-07 12:18:09 +0100

    Connector Class update

--- src/mysql/Connector.java
+++ src/mysql/Connector.java
@@ -1,7 +1,34 @@
 package mysql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+//import com.mysql.jdbc.Driver;
+
 public class Connector {
-	private final String userName = "cdio";
-	private final String password = "matador.CDIO";
-	private final String hostname = "dtu.czx5ninmk2ar.eu-west-1.rds.amazonaws.com";
+	public Connector() {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String connectionUrl = "jdbc:mysql://dtu.czx5ninmk2ar.eu-west-1.rds.amazonaws.com:3306/Matador";
+		String connectionUser = "cdio";
+		String connectionPassword = "matador.CDIO";
+			try {	
+				new com.mysql.jdbc.Driver();
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+				stmt = conn.createStatement();
+				rs = stmt.executeQuery("SELECT * FROM employees");
+				}
+			catch (Exception e) {
+				e.printStackTrace();
+				} 	
+			finally {
+				try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
+				try { if (stmt != null) stmt.close(); } catch (SQLException e) { e.printStackTrace(); }
+				try { if (conn != null) conn.close(); } catch (SQLException e) { e.printStackTrace(); }
+			}
+	}
 }
