88a6e8f1398013f83ac5076c670fd3636f7a8f39
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-03-07 11:07:13 +0100

    Connector class

---
+++ src/mysql/Connector.java
@@ -0,0 +1,7 @@
+package mysql;
+
+public class Connector {
+	private final String userName = "cdio";
+	private final String password = "matador.CDIO";
+	private final String hostname = "dtu.czx5ninmk2ar.eu-west-1.rds.amazonaws.com";
+}
