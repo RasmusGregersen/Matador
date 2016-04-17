7dbaf364627923640cc923a5e19d92572ec4c4f1
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-04-17 16:20:26 +0200

    Updated methods for ChanceCards and ChanceDeck

--- src/entity/ChanceCard.java
+++ src/entity/ChanceCard.java
@@ -2,19 +2,19 @@ package entity;
 
 public class ChanceCard {
 	private String description;
-	private int action;
+	private int effectnumber;
 	
-	public ChanceCard(String description, int action) {
+	public ChanceCard(String description, int effectnumber) {
 		this.description = description;
-		this.action = action;
+		this.effectnumber = effectnumber;
 	}
 	
 	public String getDescription() {
 		return description;
 	}
 
-	public int getAction() {
-		return action;
+	public int getEffect() {
+		return effectnumber;
 	}	
 
 }
--- src/entity/ChanceDeck.java
+++ src/entity/ChanceDeck.java
@@ -1,24 +1,43 @@
 package entity;
 
 import desktop_resources.GUI;
+
 import java.util.Random;
 
 public class ChanceDeck {
 	private ChanceCard[] deck = new ChanceCard[33];
-	private int cardCount = 0;
 	private int pickCount = 0;
+	private Random rnd = new Random();
+	
+	public ChanceDeck() {
+		CreateCards();
+		ShuffleDeck();
+	}
+	
+	public void CreateCards() {
+		deck[0] = new ChanceCard("Description",1);
+
+	}
+
+	public void ShuffleDeck() {
+		for (int i=0;i<deck.length;i++) {
+			int RandomPosition = rnd.nextInt(deck.length);
+			ChanceCard temp = deck[i];
+			deck[RandomPosition] = temp;
+		}
+	}
 	
 	public void DrawCard(Player player) {
 		GUI.showMessage("Træk et \"prøv lykken\"-kort");
 		GUI.showMessage(deck[pickCount].getDescription());
-		effect(player, deck[pickCount].getAction());
-		pickCount++;		
+		effect(player, deck[pickCount].getEffect() );
+		pickCount++;
+		if (pickCount == 33) {
+			ShuffleDeck();
+			pickCount = 0;
+		}
 	}
-	public void createCards() {
-		deck[0] = new ChanceCard("Description",1);
-		
-	}
-	
+
 	public void effect(Player player, int number) {
 		switch (number) {
 		case 1:
@@ -26,21 +45,9 @@ public class ChanceDeck {
 			player.depositBalance(1000);
 			break;
 		case 2:
-			
+
 		}
 	}
-	
-	public void shuffleDeck(ChanceCard[] deck) {
-		Random rnd = new Random();
-	    
-		for (int i = deck.length - 1; i > 0; i--) {
-	      int index = rnd.nextInt(i + 1);
-	      
-	      // Simple swap
-	      ChanceCard a = deck[index];
-	      deck[index] = deck[i];
-	      deck[i] = a;
-	    }
-	}
-	
+
 }
+
