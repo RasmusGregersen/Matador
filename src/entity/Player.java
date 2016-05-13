9d6c92ec3c056fe6c3595c62958c889ca4d3a97c
TheDave <davidjkristensen@gmail.com>
2016-05-13 14:42:22 +0200

    Quit options changed.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,20 @@
+
+/**
+ * This is the player class where player constructor is located.
+ * The player constructor is used in the Rules class where player is constructed
+ *
+ * @author Group 2
+ * @version 1.0
+ * @Since 13/5-2016
+ *
+ * */
 package entity;
 
 import desktop_resources.GUI;
+
+/**
+ * Private player variables
+ * */
 
 public class Player {
     private String name;
@@ -14,6 +28,21 @@ public class Player {
     private boolean jailed;
     private int ExtraTurns;
 
+    /**
+     * Player constructor
+     *
+     *@Param name The name of the actual player
+     *@Param balance The balance of the actual player
+     *@Param TotalAssets Value of all house, hotel and properties. Used for specific chancecard
+     *@Param FieldPos Players position on the gameboard
+     *@Param breweries Number of breweries
+     *@Param shipping Number of owned shipping properties
+     *@Param jailcard Number of jailcard in player possession
+     *@Param Jailturns Number of many times player has been in jail
+     *@Param jailed Jailed boolean which it set either true or false. False as standard
+     *
+     * */
+
     public Player(String name, int balance, int TotalAssets, int FieldPos, int breweries, int shipping, int jailcard, int Jailturns, boolean jailed) {
         this.name = name;
         this.balance = balance;
@@ -26,6 +55,11 @@ public class Player {
         this.jailed = jailed;
     }
 
+    /**
+     * Getters and setters to seek information about the constructed players
+     *
+     * */
+
     public int getJailturns() {
         return Jailturns;
     }
@@ -37,6 +71,12 @@ public class Player {
     public boolean isJailed() {
         return jailed;
     }
+
+    /**
+     * Setter with special condition
+     * If jailed is true, the program moves car to jail position
+     * If jailed is false, jailturns which is a counter is set to 0
+     * */
 
     public void setJailed(boolean jailed) {
         this.jailed = jailed;
@@ -53,6 +93,8 @@ public class Player {
         }
     }
 
+    /** Getters and setters for extra turn */
+
     public int getExtraTurns() {
         return ExtraTurns;
     }
@@ -66,28 +108,44 @@ public class Player {
         return balance;
     }
 
-    // Method to withdraw from the balance
+    /**
+     *
+     * Method to withdraw from the balance
+     *
+     * */
     public void withdrawBalance(int i) {
         balance = balance - i;
         GUI.setBalance(name, balance);
     }
 
-    // Method to deposit from the balance
+    /**
+     *
+     * Method to deposit from the balance
+     *
+     * */
     public void depositBalance(int i) {
         balance = balance + i;
         GUI.setBalance(name, balance);
     }
 
-    // method to return player name.
+    /**
+     *
+     *  method to return player name.
+     *
+     * */
     public String getName() {
         return name;
     }
 
-    // method to set player name.
+    /**
+     *
+     * Methods to set and get player information
+     *
+     * */
+
     public void setName(String name) {
         this.name = name;
     }
-
     public int getShipping() {
         return shipping;
     }
@@ -117,6 +175,13 @@ public class Player {
         this.jailcard = jailcard;
     }
 
+    /**
+     *
+     * Setter for field position with check for start field
+     * Checks if player has passed the start position
+     *
+     * */
+
     public void setFieldPos(int FieldPos) {
         if (this.FieldPos > FieldPos) {
             GUI.showMessage("Du modtager hermed kr. 4000,- for at passere start");
@@ -127,6 +192,13 @@ public class Player {
         GUI.removeAllCars(name);
         GUI.setCar(this.FieldPos, name);
     }
+
+    /**
+     *
+     * Method to move player
+     * Checks if player has passed the start position
+     *
+     * */
 
     public void moveToFieldPos(int FieldPos) {
         this.FieldPos = FieldPos + this.FieldPos;
@@ -140,12 +212,19 @@ public class Player {
         GUI.setCar(this.FieldPos, name);
     }
 
+    /**
+     *
+     * Getters and setters for TotalAssets
+     * Setter is int value added to TotalAsset
+     *
+     * */
+
     public int getTotalAssets() {
         return TotalAssets;
     }
 
-    public void setTotalAssets(int totalAssets) {
-        TotalAssets = totalAssets + TotalAssets;
+    public void setTotalAssets(int TotalAssets) {
+        this.TotalAssets = this.TotalAssets + TotalAssets;
     }
 
 }
