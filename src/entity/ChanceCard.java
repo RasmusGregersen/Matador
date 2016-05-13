f126f49ad938adf810e22f8158fef30b9d9bdb2b
Rasmus Gregersen <rasmus.gregersen@live.dk>
2016-05-13 13:59:53 +0200

    Code Cleanup.

--- src/entity/ChanceCard.java
+++ src/entity/ChanceCard.java
@@ -1,16 +1,23 @@
 package entity;
 
-//Chancecard class
+/**
+ * Our ChanceCard class.
+ */
 class ChanceCard {
     private final String description;
     private final int effectnumber;
 
-    //The constructor for the ChanceCards
+    /**
+     * The constructor for the ChanceCards
+     */
     public ChanceCard(String description, int effectnumber) {
         this.description = description;
         this.effectnumber = effectnumber;
     }
 
+    /**
+     * The constructor for the ChanceCards
+     */
     public String getDescription() {
         return description;
     }
